56e918e224db3ed21c04bf23427c35654a4cc0c8
erreur404forreal <ceremej@gmail.com>
2022-05-19 16:27:56 +0200

    ✨  : map through received data to display dynamic ImageOptions on the screen

--- App.js
+++ App.js
@@ -2,14 +2,22 @@ import React from "react";
 import { Text,View, Image, TextInput } from "react-native";
 import styles from "./App.styles";
 import ImageOption from "./src/components/ImageOption/";
+import question from "./assets/data/oneQuestionWithOption";
 
 const App = () =>{
 
   return (
     <View style={styles.root}>
-      <Text style={styles.title}> Which of these is the "glass" ?</Text>
+      <Text style={styles.title}>{question.question}</Text>
       <View style={styles.optionsContainer}>
-        <ImageOption />
+
+        {question.options.map((option) =>
+          <ImageOption 
+          key={option.id}
+          image={option.image}
+          text={option.text}
+          />
+        )}
         
       </View>
     </View>
--- src/components/ImageOption/ImageOption.js
+++ src/components/ImageOption/ImageOption.js
@@ -2,18 +2,15 @@ import React from "react";
 import { Text,View, Image } from "react-native";
 import styles from "./styles";
 
-const ImageOption = () => {
-  return (
+const ImageOption = ({image, text}) => (
     <View style={styles.optionContainer}>
       <Image source={{ 
-          uri: "https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/cup.png"
+          uri: image
         }}
         resizeMode="contain"
         style={styles.optionImage}
       />
-      <Text Text style={styles.optionText}> glass</Text>
+      <Text Text style={styles.optionText}>{text}</Text>
     </View>
-
-  )
-}
+  );
 export default ImageOption;
