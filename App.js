306e0fa65e23a10ff6cbecf710e9b3ae1d28d304
erreur404forreal <ceremej@gmail.com>
2022-07-18 16:26:48 +0200

    ✨ : store progress with AsyncStorage

--- App.js
+++ App.js
@@ -7,13 +7,16 @@ import Header from "./src/components/Header";
 
 
 import questions from "./assets/data/allQuestions";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ActivityIndicator } from "react-native";
 
 const App = () =>{
 
   const [selected, setSelected] = useState(null);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [currentQuestion, setCurrentQuestion] = useState(questions[currentQuestionIndex]);
-  const [lives, setLives] = useState(5)
+  const [lives, setLives] = useState(5);
+  const [hasLoaded, setHasLoaded] = useState(false);
 
   useEffect(() => {
     if(currentQuestionIndex >= questions.length) {
@@ -23,6 +26,16 @@ const App = () =>{
       setCurrentQuestion(questions[currentQuestionIndex]);
     }
   }, [currentQuestionIndex]);
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  useEffect(() => {
+    if(hasLoaded) {
+      saveData();
+    }
+  }, [lives, currentQuestionIndex, hasLoaded]);
 
 const onCorrect = () => {
   setCurrentQuestionIndex(currentQuestionIndex + 1);
@@ -48,6 +61,29 @@ const onIncorrect = () => {
   }
 }
 
+const saveData = async () => {
+  await AsyncStorage.setItem('lives', lives.toString());
+  await AsyncStorage.setItem('currentQuestionIndex', currentQuestionIndex.toString());
+}
+
+const loadData = async () => {
+  const loadedLives = await AsyncStorage.getItem("lives");
+  if(loadedLives) {
+    setLives(parseInt(loadedLives));
+  }
+
+  const currentQuestionIndex = await AsyncStorage.getItem("currentQuestionIndex");
+  if(currentQuestionIndex) {
+    setCurrentQuestionIndex(parseInt(currentQuestionIndex));
+  }
+  setHasLoaded(true);
+}
+
+if (!hasLoaded) {
+  return (
+    <ActivityIndicator />
+  );
+}
   return (
     <SafeAreaView style={styles.container}>
     <View style={styles.root}>
